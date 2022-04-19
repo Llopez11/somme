1237e55fa85ed2ed7444caf9a9bc84298a18c216
Llopez11 <104028929+Llopez11@users.noreply.github.com>
2022-04-19 20:36:30 +0200

    Add files via upload

---
+++ somme.py
@@ -0,0 +1,8 @@
+#On ecrit le premier nombre
+nb1 = int(input("Saisir un nombre : "))
+#On ecrit le second nombre
+nb2 = int(input("Saisir un nombre : "))
+#On fait la somme
+somme = nb1 + nb2
+#On affiche le resultat
+print ("La somme des nombres vaut : ", somme)
